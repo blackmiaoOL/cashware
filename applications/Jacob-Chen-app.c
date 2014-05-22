3e9572b4f6a074e9c2a849c8d424ee3d0f0159b7
陈豪 <547884940@qq.com>
2014-05-23 00:21:39 +0800

    add_ini
    读写示范在jacob-chen-app.c里 没有启用
    用之前先放文件

--- applications/Jacob-Chen-app.c
+++ applications/Jacob-Chen-app.c
@@ -1,17 +1,40 @@
 #include "stm32f4xx.h"
 #include <board.h>
 #include <rtthread.h>
+#include "inifile.h"
+
+#define BUF_SIZE 256\
 
 void rt_app_entry(void *parameter)
 {
+    const char *file ="profile.ini";//这里也可以改成“myconfig,txt”
+    const char *section = "Service";
+    const char *key = "Name";
+    char rec[BUF_SIZE]={0};
+
 //    while (USART_GetFlagStatus(USART6, USART_FLAG_RXNE) == RESET);
 //        rt_kprintf("fuck\n");  
 //    while(1)
 //    {
 //        rt_thread_delay(2);
 //    }
+    
+#ifdef USE_INI //make sure you have profile.ini
+//    if(!write_profile_string(section,key,"Tony",file))
+//    {
+//        rt_kprintf("write name pair to ini file fail\n");
+//    }
+    if(!read_profile_string(section, key, rec, BUF_SIZE,"",file))
+    {
+        rt_kprintf("read ini file fail\n");
+    }
+    else
+    {
+        rt_kprintf("%s=%s\n",key,rec);
+    }
+#endif
+    
 }
-
 
 
 void Jacob_appinit()
@@ -20,7 +43,7 @@ void Jacob_appinit()
 
     tid = rt_thread_create("jacob",
                            rt_app_entry, RT_NULL,
-                           4048, 10, 20);
+                           8048, 20, 20);
     
     
     if (tid != RT_NULL)
